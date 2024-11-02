c96de794512321c14f7459c84afad1ff441c38dd
Mohanraja Loganathan <mohl@sitecore.net>
2024-11-02 11:51:30 +0530

    Adding provider view model s

--- src/Recon.UI/wwwroot/js/master/providers.js
+++ src/Recon.UI/wwwroot/js/master/providers.js
@@ -1,7 +1,32 @@
-﻿function AppViewModel() {
-    this.message = ko.observable("Hello, Knockout!");
-}
+﻿(function (jq, kn) {
 
-// Apply the bindings to activate Knockout
-const viewModel = new AppViewModel();
-ko.applyBindings(viewModel);
+    var baseAddress = window.location.origin;
+
+    async function loadProvider() {
+
+        const url = baseAddress + "/api/provider";
+
+        try {
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const providers = await response.json();
+
+            return providers;
+
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    var providerViewModel = function (providers) {
+
+    }
+
+    jq(loadProvider);
+
+})($, ko);
+
